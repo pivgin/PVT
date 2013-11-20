f44ac4267d2eca4207b5e5b77f677b98c207a170
pivgin <pivgin@gmail.com>
2013-11-21 01:15:06 +0300

    ConsApp was created

---
+++ CApp1_les1/CApp1_les1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CApp1_les1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Max is here");
+            Console.WriteLine("Hi");
+            Console.ReadKey();
+        }
+    }
+}
