6d5ffcde9d65838203ca5b54fba92d6d4f4a30ca
pivgin <pivgin@gmail.com>
2013-11-26 00:54:17 +0300

    almost finished, without inheritanse

--- CApp_Les2_HW_FileSort/CApp_Les2_HW_FileSort/Program.cs
+++ CApp_Les2_HW_FileSort/CApp_Les2_HW_FileSort/Program.cs
@@ -11,38 +11,83 @@ namespace CApp_Les2_HW_FileSort
     {
         class AutoSort
         {
-            private Sort()
+            public static int[] Sort(int [] intnumberscopy)
             {
-
+                Array.Sort(intnumberscopy);
+                return intnumberscopy;
             }
+
         }
 
         class CustomSort
         {
-            private Sort()
-            {
 
+            public static int[] Sort(int[] intnumbers)
+            {
+                //int [] strnumberscopy;
+                int[] intnumberscopy = intnumbers;
+                int temp;
+
+                for (int i = 0; i < intnumberscopy.Length - 1; i++) 
+                {
+                    for (int j = 0; j < intnumberscopy.Length - i - 1; j++)
+                    {
+                        if (intnumberscopy[j] > intnumberscopy[j + 1]) 
+                        {
+                            temp = intnumberscopy[j];
+                            intnumberscopy[j] = intnumberscopy[j+1];
+                            intnumberscopy[j + 1] = temp;
+
+                        }
+                    }
+                }
+
+
+                    return intnumberscopy;
             }
 
         }
 
         static void Main(string[] args)
         {
-            File.OpenRead("data\\originalf.txt");
-            string [] origstr = File.ReadAllLines("originalf.txt");
-            int[] strnumbers = new int[origstr.Length];
-
+            //string path = @"C:\Users\Pivgin\Source\Repos\PVT\CApp_Les2_HW_FileSort\CApp_Les2_HW_FileSort\bin\Debug\data";
+            //File.OpenRead("data\\originalf.txt");
             
-
-            File.OpenWrite("data\\resultf.txt");
-            StreamWriter streamwr = new StreamWriter("data\\resultf.txt");
-            streamwr.WriteLine("Custom Sort");
-            streamwr.WriteLine(strnumbers);
-            streamwr.WriteLine();
-            streamwr.Close();
-
-            //StreamReader streamrd = new StreamReader();
-            File.Delete("data\\resultf.txt");
+            string [] origstr = File.ReadAllLines("data\\originalf.txt");
+            int[] intnumbers = new int[origstr.Length];
+            int[] intnumberscopy = new int[origstr.Length];
+            string[] strnumbers = new string[origstr.Length];
+            int i=0;
+            string[] stri = new string[origstr.Length];
+            foreach (string s in origstr) 
+            {
+                intnumbers[i] = int.Parse(s);
+                intnumberscopy[i] = int.Parse(s);
+                i++;
+            }
+            //int[] intnumberscopy = intnumbers;
+            CustomSort.Sort(intnumbers);
+           
+            i = 0;
+            foreach (int s1 in intnumbers) 
+            {
+                strnumbers[i] = s1.ToString();
+                Console.WriteLine(strnumbers[i]);
+                File.WriteAllLines("data\\resultf1.txt", strnumbers);
+                i++;
+            }
+            Console.WriteLine("\n" + "----------");
+            AutoSort.Sort(intnumberscopy);
+            i = 0;
+            foreach (int s1 in intnumberscopy)
+            {
+                strnumbers[i] = s1.ToString();
+                Console.WriteLine(strnumbers[i]);
+                File.WriteAllLines("data\\resultf2auto.txt", strnumbers);
+                i++;
+            }
+            Console.ReadKey();
+           // File.Delete("data\\resultf1.txt");
         }
     }
 }
