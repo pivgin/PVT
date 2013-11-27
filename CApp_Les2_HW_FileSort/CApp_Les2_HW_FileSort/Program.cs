6e6cf5147033ca7c73a54d7eeccc6235e9edc331
pivgin <pivgin@gmail.com>
2013-11-27 17:10:28 +0300

    With MyExeption, doesn't work properly

--- CApp_Les2_HW_FileSort/CApp_Les2_HW_FileSort/Program.cs
+++ CApp_Les2_HW_FileSort/CApp_Les2_HW_FileSort/Program.cs
@@ -46,6 +46,11 @@ namespace CApp_Les2_HW_FileSort
         }
 
     }
+
+    class MyException : Exception 
+    {
+        public int Stringnumber { get; set; }
+    }
     
     class Program
     {
@@ -55,55 +60,73 @@ namespace CApp_Les2_HW_FileSort
             //File.OpenRead("data\\originalf.txt");
           string fName = Console.ReadLine();
 
-            try
+          try
+          {
+              if (!File.Exists(fName))    // data/originalf.txt
+              {
+                  throw new FileNotFoundException();
+              }
+
+              string[] origstr = File.ReadAllLines(fName);
+              int[] intnumbers = new int[origstr.Length];
+              int[] intnumberscopy = new int[origstr.Length];
+              string[] strnumbers = new string[origstr.Length];
+              int i = 0;
+              string[] stri = new string[origstr.Length];
+              
+              
+                  foreach (string s in origstr)
+                  { 
+                      try
+                        {
+                            intnumbers[i] = int.Parse(s);
+                            intnumberscopy[i] = int.Parse(s);
+                            i++;
+                          throw new MyException() { Stringnumber = i };
+                        }
+                      catch (MyException exception)
+                      {
+                          Console.WriteLine("Not all the numbers in the source file are numbers.");
+                          Console.WriteLine("Please check all of them. Thx");
+                          Console.WriteLine("Wrong value is in string " + exception.Stringnumber);
+                      }
+                      
+                      //intnumbers[i] = int.Parse(s);
+                      //intnumberscopy[i] = int.Parse(s);  
+                      //i++;
+                    
+                  }
+              
+              CustomSort.Sort(intnumbers);
+
+              i = 0;
+              foreach (int s1 in intnumbers)
+              {
+                  strnumbers[i] = s1.ToString();
+                  Console.WriteLine(strnumbers[i]);
+                  File.WriteAllLines("data\\resultf1.txt", strnumbers);
+                  i++;
+              }
+              
+              Console.WriteLine("\n" + "----------");
+              AutoSort.Sort(intnumberscopy);
+              
+              i = 0;
+              foreach (int s1 in intnumberscopy)
+              {
+                  strnumbers[i] = s1.ToString();
+                  Console.WriteLine(strnumbers[i]);
+                  File.WriteAllLines("data\\resultf2auto.txt", strnumbers);
+                  i++;
+              }
+
+          }
+          catch (FileNotFoundException exception)
             {
-                if (!File.Exists(fName))    // data/originalf.txt
-                {   
-                    throw new FileNotFoundException();
-                }
-                
-                string[] origstr = File.ReadAllLines(fName);
-                int[] intnumbers = new int[origstr.Length];
-                int[] intnumberscopy = new int[origstr.Length];
-                string[] strnumbers = new string[origstr.Length];
-                int i = 0;
-                string[] stri = new string[origstr.Length];
-
-                foreach (string s in origstr)
-                {
-                    intnumbers[i] = int.Parse(s);
-                    intnumberscopy[i] = int.Parse(s);
-                    i++;
-                }
-                //int[] intnumberscopy = intnumbers;
-                CustomSort.Sort(intnumbers);
-
-                i = 0;
-                foreach (int s1 in intnumbers)
-                {
-                    strnumbers[i] = s1.ToString();
-                    Console.WriteLine(strnumbers[i]);
-                    File.WriteAllLines("data\\resultf1.txt", strnumbers);
-                    i++;
-                }
-                Console.WriteLine("\n" + "----------");
-                AutoSort.Sort(intnumberscopy);
-                i = 0;
-                foreach (int s1 in intnumberscopy)
-                {
-                    strnumbers[i] = s1.ToString();
-                    Console.WriteLine(strnumbers[i]);
-                    File.WriteAllLines("data\\resultf2auto.txt", strnumbers);
-                    i++;
-                }
-
-            }
-            catch (FileNotFoundException exception)
-            {
-                Console.WriteLine("Man! "+ exception.Message);
+              Console.WriteLine("Man! " + exception.Message);
             }
 
-            Console.ReadKey();
+          Console.ReadKey();
            // File.Delete("data\\resultf1.txt");
         }
     }
